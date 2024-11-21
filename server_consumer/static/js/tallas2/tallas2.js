d3265907545861bec147ed5c7cfe10f52b57da51
dolganin <8yp3r1@gmail.com>
2024-11-22 01:37:34 +0700

    Remote mode is working now. Maybe

--- server_consumer/static/js/tallas2/tallas2.js
+++ server_consumer/static/js/tallas2/tallas2.js
@@ -2,27 +2,36 @@ document.addEventListener("DOMContentLoaded", function() {
     var socket = io();  // Подключение к WebSocket
     
     socket.on('new_frame', function(data) {
-        console.log('Received new frame:', data);
-        var img = document.getElementById('stream');
-        img.src = 'data:image/png;base64,' + data.image;
-        console.log('Updated image src:', img.src);
-
-        document.getElementById('epoch').innerText = 'Epoch: ' + data.epoch;
-        document.getElementById('loss').innerText = 'Loss: ' + data.loss;
-        document.getElementById('meanReward').innerText = 'Mean Reward: ' + data.meanReward;
-        document.getElementById('mode').innerText = 'Mode: ' + data.mode;
+        const host = (data && data.host) ? data.host.toString().trim().toLowerCase() : 'unknown';
+        console.log('Received new frame from host:', host);
+        
+        if (host === 'tallas2' && data.image) {
+            var img = document.getElementById('stream');
+            img.src = 'data:image/png;base64,' + data.image;
+            console.log('Updated image src:', img.src);
+            
+            document.getElementById('epoch').innerText = 'Epoch: ' + data.epoch;
+            document.getElementById('loss').innerText = 'Loss: ' + data.loss;
+            document.getElementById('meanReward').innerText = 'Mean Reward: ' + data.meanReward;
+            document.getElementById('mode').innerText = 'Mode: ' + data.mode;
+        } else {
+            console.log('Ignoring frame from host:', host);
+            if (!data.image) {
+                console.log('Frame does not contain image data.');
+            }
+        }
     });
-
+    
     socket.on('connect', function() {
         console.log('Connected to server');
-        socket.emit('join', { page: 'doom_screen' });
-        console.log('Joined doom_screen room');
+        socket.emit('join', { page: 'tallas2' });
+        console.log('Joined talls2 room');
     });
-
+    
     socket.on('disconnect', function() {
         console.log('Disconnected from server');
     });
-
+    
     socket.on('connect_error', function(error) {
         console.error('Connection Error:', error);
     });
