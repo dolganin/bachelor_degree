86a572ac9c02a3cc4580843ef1e248a27595dbd4
dolganin <8yp3r10@gmail.com>
2025-05-24 16:29:44 +0700

    Reduced the vs

--- server_consumer/broker_kafka.py
+++ server_consumer/broker_kafka.py
@@ -6,7 +6,7 @@ import cv2
 import logging
 
 # Configure logging
-logging.basicConfig(level=logging.ERROR)
+logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
 # Kafka producer configuration
