478e435344d96fc47cc02ed388bc3c5541d504f4
dolganin <8yp3r10@gmail.com>
2025-05-24 16:59:38 +0700

    Changed to Bernoulli distrib

--- ppo/pnetwork.py
+++ ppo/pnetwork.py
@@ -3,20 +3,15 @@ import torch
 
 class PolicyNetwork(nn.Module):
     def __init__(self, shared_transformer: nn.Module, action_dim: int, hidden_dim: int = 64):
-        super(PolicyNetwork, self).__init__()
+        super().__init__()
         self.shared_transformer = shared_transformer
-        self.fc_mean = nn.Sequential(
+        self.fc = nn.Sequential(
             nn.Linear(shared_transformer.embedding_dim, hidden_dim),
             nn.ReLU(),
             nn.Linear(hidden_dim, action_dim)
         )
-        self.fc_log_std = nn.Parameter(torch.zeros(action_dim))
-    
     def forward(self, state: torch.Tensor):
         features = self.shared_transformer(state)  # (batch_size, embedding_dim)
-        mean = self.fc_mean(features)  # (batch_size, action_dim)
-        batch_size = mean.size(0)
-        # Вместо expand_as используем repeat для создания нового тензора
-        log_std = self.fc_log_std.repeat(batch_size, 1)
-        std = torch.exp(log_std)
-        return mean, std
+        logits = self.fc(features)  # (batch_size, action_dim), без активации
+        return logits
+
--- ppo/ppo_agent.py
+++ ppo/ppo_agent.py
@@ -73,13 +73,14 @@ class PPOAgent(RLAgent):
         state = torch.FloatTensor(state).unsqueeze(0).to(self.device)
         self.policy_net.eval()
         with torch.no_grad():
-            mean, std = self.policy_net(state)  # shape (1, action_dim)
-            std = torch.clamp(std, min=1e-6, max=1.0)
-            dist = torch.distributions.Normal(mean, std)
+            logits = self.policy_net(state)  # (1, action_dim)
+            probs = torch.sigmoid(logits)
+            dist = torch.distributions.Bernoulli(probs)
             action = dist.sample()
             action_log_prob = dist.log_prob(action).sum(dim=-1)
         self.policy_net.train()
-        return action.cpu().numpy()[0], action_log_prob.cpu()
+        return action.cpu().numpy()[0], action_log_prob.cpu()  # тензор
+
 
 
     
